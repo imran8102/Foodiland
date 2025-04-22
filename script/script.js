03674c4b4a654e4faf3c768ce1824d5421e63ab9
imran <emranbagheri81@gmail.com>
2025-04-22 22:06:17 +0330

    fix(categories):improve responsive design adjustments;feat:add recipes section to home page

--- script/script.js
+++ script/script.js
@@ -7,45 +7,58 @@ let barsMenu = document.querySelector(".barsMenu")
 let contactBtn = document.querySelector('.contactBtn')
 let contactMenu = document.querySelector(".contactMenu")
 let container = document.querySelector(".container")
+let heartIcon = document.querySelectorAll(".heart")
 
-dropDownBtn.addEventListener("click",() => {
-    if(window.innerWidth < 768){
+dropDownBtn.addEventListener("click", () => {
+    if (window.innerWidth < 768) {
         dropDownMenu.classList.toggle("hidden")
     }
 })
 
-window.addEventListener("resize",() => {
-    if(window.innerWidth >= 768){
+window.addEventListener("resize", () => {
+    if (window.innerWidth >= 768) {
         mainDropDown.classList.add("max-md:hidden")
         closeBtn.classList.add("hidden")
         barsMenu.classList.remove("hidden")
         dropDownMenu.classList.add("hidden")
     }
-        
+
 })
 
-    toggleBtn.addEventListener("click",() => {
-        mainDropDown.classList.toggle("max-md:hidden")
-        if(!dropDownMenu.classList.contains("hidden"))
-            dropDownMenu.classList.add("hidden")
-        closeBtn.classList.toggle("hidden")
-        barsMenu.classList.toggle("hidden")
-    })
+toggleBtn.addEventListener("click", () => {
+    mainDropDown.classList.toggle("max-md:hidden")
+    if (!dropDownMenu.classList.contains("hidden"))
+        dropDownMenu.classList.add("hidden")
+    closeBtn.classList.toggle("hidden")
+    barsMenu.classList.toggle("hidden")
+})
 
 
-contactBtn.addEventListener("click",()=>{
+contactBtn.addEventListener("click", () => {
     contactMenu.classList.toggle("max-md:hidden")
 })
 
 //Checking for clicks outside the dropdown area(or outside of dropdownBtn). 🚀
-window.addEventListener("click",(e) => {
-    if(!contactMenu.contains(e.target) && !contactBtn.contains(e.target))
+window.addEventListener("click", (e) => {
+    if (!contactMenu.contains(e.target) && !contactBtn.contains(e.target))
         contactMenu.classList.add("max-md:hidden")
-    if(!mainDropDown.contains(e.target) && !dropDownMenu.classList.contains("max-md:hidden") && !toggleBtn.contains(e.target)){
+    if (!mainDropDown.contains(e.target) && !dropDownMenu.classList.contains("max-md:hidden") && !toggleBtn.contains(e.target)) {
         mainDropDown.classList.add("max-md:hidden")
-        if(!dropDownMenu.classList.contains("hidden"))
+        if (!dropDownMenu.classList.contains("hidden"))
             dropDownMenu.classList.add("hidden")
         closeBtn.classList.add("hidden")
         barsMenu.classList.remove("hidden")
     }
+})
+
+heartIcon.forEach((heart) =>{
+    heart.addEventListener("click", () => {
+        if (heart.classList.contains("fill-[#FF6363]")) {
+            heart.classList.add("fill-[#dbe2e5]")
+            heart.classList.remove("fill-[#FF6363]")
+        }else{
+            heart.classList.remove("fill-[#dbe2e5]")
+            heart.classList.add("fill-[#FF6363]")
+        }
+    })
 })
