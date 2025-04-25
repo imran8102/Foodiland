74f4109519a5fe04dfc2c52876e97f6ee651564b
imran <emranbagheri81@gmail.com>
2025-04-25 10:32:48 +0330

    fix: fixed some issues on the navbar

--- script/script.js
+++ script/script.js
@@ -6,7 +6,6 @@ let closeBtn = document.querySelector(".closeBtn")
 let barsMenu = document.querySelector(".barsMenu")
 let communicationsBtn = document.querySelector('.communicationsBtn')
 let communicationsMenu = document.querySelector(".communicationsMenu")
-let container = document.querySelector(".container")
 let heartIcon = document.querySelectorAll(".heart")
 
 //Closing the dropdown menu when the screen size exceeds medium.
@@ -17,22 +16,24 @@ window.addEventListener("resize", () => {
         barsMenu.classList.remove("hidden")
         dropDownMenu.classList.remove("max-md:hidden")
     }else{
-        mainDropDown.classList.add("max-md:hidden")
+        // mainDropDown.classList.add("max-md:hidden") => bug #1 : Replace with the two lines below.
+        mainDropDown.classList.remove("max-md:scale-y-100")
+        mainDropDown.classList.add("max-md:scale-y-0")
         closeBtn.classList.add("hidden")
         barsMenu.classList.remove("hidden")
     }
+    
 })
 
 
-// bug : first transition after resize donsn't work
+// bug : first transition after resize donsn't work => fixed #1
 //toggle bars menu
 toggleBtn.addEventListener("click", () => {
     if (!dropDownMenu.classList.contains("max-md:hidden"))
         dropDownMenu.classList.add("max-md:hidden")
-    if(mainDropDown.classList.contains("max-md:hidden"))
-        mainDropDown.classList.remove("max-md:hidden")
     
     if (mainDropDown.classList.contains("max-md:scale-y-0")) {
+        mainDropDown.classList.remove("max-md:hidden")
         mainDropDown.classList.remove("max-md:scale-y-0")
         mainDropDown.classList.add("max-md:scale-y-100")
     } else {
@@ -54,25 +55,27 @@ dropDownBtn.addEventListener("click", () => {
 
 //Checking for clicks outside the dropdown area(or outside of dropdownBtn). 🚀
 window.addEventListener("click", (e) => {
-    if (!communicationsMenu.contains(e.target) && !communicationsBtn.contains(e.target))
-        communicationsMenu.classList.add("max-md:hidden")
-    if (!mainDropDown.contains(e.target) && !dropDownMenu.classList.contains("max-md:hidden") && !toggleBtn.contains(e.target)) {
-        // mainDropDown.classList.add("max-md:hidden")
-        // if (!dropDownMenu.classList.contains("hidden"))
-        //     dropDownMenu.classList.add("hidden")
+    if (!communicationsMenu.contains(e.target) && !communicationsBtn.contains(e.target) && communicationsMenu.classList.contains("max-md:opacity-100")){
+        communicationsMenu.classList.remove("max-md:scale-y-100")
+        communicationsMenu.classList.remove("max-md:opacity-100")
+        communicationsMenu.classList.add("max-md:scale-y-90")
+        communicationsMenu.classList.add("max-md:opacity-0")
+    }
+    if (!mainDropDown.contains(e.target) && !toggleBtn.contains(e.target)) {
+        mainDropDown.classList.remove("max-md:scale-y-100")
+        mainDropDown.classList.add("max-md:scale-y-0")
         closeBtn.classList.add("hidden")
         barsMenu.classList.remove("hidden")
     }
 })
 
-//bug:it dosn't works after open up other dropdowns
+//bug:it dosn't works after click outside of it => fixed #2
 //toggle communications menu
 communicationsBtn.addEventListener("click", () => {
-    // communicationsMenu.classList.toggle("max-md:hidden")
     if (communicationsMenu.classList.contains("max-md:scale-y-90")) {
         communicationsMenu.classList.remove("max-md:scale-y-90")
-        communicationsMenu.classList.add("max-md:scale-y-100")
         communicationsMenu.classList.remove("max-md:opacity-0")
+        communicationsMenu.classList.add("max-md:scale-y-100")
         communicationsMenu.classList.add("max-md:opacity-100")
     }else{
         communicationsMenu.classList.remove("max-md:scale-y-100")
@@ -80,7 +83,6 @@ communicationsBtn.addEventListener("click", () => {
         communicationsMenu.classList.add("max-md:scale-y-90")
         communicationsMenu.classList.add("max-md:opacity-0")
     }
-
 })
 
 //give heart for each recipes card
