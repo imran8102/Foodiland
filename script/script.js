87e0b47de2982d40b6563e7ac35af77f1ae8f4cf
imran <emranbagheri81@gmail.com>
2025-04-27 23:26:35 +0330

    feat:add dynamic articles from js and implement static pagination

--- script/script.js
+++ script/script.js
@@ -8,6 +8,146 @@ let communicationsBtn = document.querySelector('.communicationsBtn')
 let communicationsMenu = document.querySelector(".communicationsMenu")
 let heartIcon = document.querySelectorAll(".heart")
 let radioButtons = document.querySelectorAll(".radioBtn")
+let articlesList = document.querySelector(".articlesList")
+let pagination = document.querySelectorAll('.pageBtn')
+let paginationList = document.querySelector(".paginationList")
+let currentPage = 1
+const rowsCount = 6
+
+let currentBtn = document.querySelector('button[value="1"]')
+let previousBtn = document.querySelector(".previousBtn")
+let nextBtn = document.querySelector(".nextBtn")
+let moreBtn = document.querySelector(".moreBtn")
+
+let articles = [
+    {
+        id : 1,
+        image:"soup",
+        title: "Crochet Projects for Noodle Lovers",
+        description:"Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ",
+        profilePic:"Ellipse 3",
+        authorName:"Wade Warren",
+        date:"26 April 2025"
+    },
+    {
+        id : 2,
+        image:"fruit",
+        title: "10 Vegetarian Recipes To Eat This Month",
+        description:"Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ",
+        profilePic:"Ellipse 4",
+        authorName:"Robert Fox",
+        date:"26 April 2025"
+    },
+    {
+        id : 3,
+        image:"chef",
+        title: "Full Guide to Becoming a Professional Chef",
+        description:"Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ",
+        profilePic:"Ellipse 5",
+        authorName:"Dianne Russell",
+        date:"26 April 2025"
+    },
+    {
+        id : 4,
+        image:"Lasagna",
+        title: "Simple & Delicious Vegetarian Lasagna",
+        description:"Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ",
+        profilePic:"Ellipse 6",
+        authorName:"Leslie Alexander",
+        date:"26 April 2025"
+    },
+    {
+        id : 5,
+        image:"plantain",
+        title: "Plantain and Pinto Stew with Aji Verde",
+        description:"Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ",
+        profilePic:"Ellipse 7",
+        authorName:"Courtney Henry",
+        date:"26 April 2025"
+    },
+    {
+        id : 6,
+        image:"assistant",
+        title: "We’re Hiring a Communications Assistant!",
+        description:"Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ",
+        profilePic:"Ellipse 8",
+        authorName:"Albert Flores",
+        date:"26 April 2025"
+    },
+    {
+        id : 7,
+        image:"assistant",
+        title: "We’re Hiring a Communications Assistant!",
+        description:"Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ",
+        profilePic:"Ellipse 8",
+        authorName:"Albert Flores",
+        date:"26 April 2025"
+    },
+    {
+        id : 8,
+        image:"assistant",
+        title: "We’re Hiring a Communications Assistant!",
+        description:"Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ",
+        profilePic:"Ellipse 8",
+        authorName:"Albert Flores",
+        date:"26 April 2025"
+    },
+    {
+        id : 9,
+        image:"assistant",
+        title: "We’re Hiring a Communications Assistant!",
+        description:"Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ",
+        profilePic:"Ellipse 8",
+        authorName:"Albert Flores",
+        date:"26 April 2025"
+    },
+    {
+        id : 10,
+        image:"assistant",
+        title: "We’re Hiring a Communications Assistant!",
+        description:"Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ",
+        profilePic:"Ellipse 8",
+        authorName:"Albert Flores",
+        date:"26 April 2025"
+    },
+    {
+        id : 11,
+        image:"assistant",
+        title: "We’re Hiring a Communications Assistant!",
+        description:"Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ",
+        profilePic:"Ellipse 8",
+        authorName:"Albert Flores",
+        date:"26 April 2025"
+    },
+    {
+        id : 12,
+        image:"assistant",
+        title: "We’re Hiring a Communications Assistant!",
+        description:"Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ",
+        profilePic:"Ellipse 8",
+        authorName:"Albert Flores",
+        date:"26 April 2025"
+    },
+    {
+        id : 13,
+        image:"assistant",
+        title: "We’re Hiring a Communications Assistant!",
+        description:"Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ",
+        profilePic:"Ellipse 8",
+        authorName:"Albert Flores",
+        date:"26 April 2025"
+    },
+    {
+        id : 14,
+        image:"assistant",
+        title: "We’re Hiring a Communications Assistant!",
+        description:"Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ",
+        profilePic:"Ellipse 8",
+        authorName:"Albert Flores",
+        date:"26 April 2025"
+    }
+]
+
 
 //Closing the dropdown menu when the screen size exceeds medium.
 window.addEventListener("resize", () => {
@@ -113,7 +253,6 @@ radioButtons.forEach((radioButton) => {
         if(e.target.classList.contains("temp")){
             e.target.classList.add("hidden")
             e.target.previousElementSibling.classList.remove("hidden")
-            console.log(e.target.parentElement.nextElementSibling)
             e.target.parentElement.nextElementSibling.classList.add("line-through")
             e.target.parentElement.nextElementSibling.classList.add("text-[#00000060]")
         }
@@ -125,3 +264,77 @@ radioButtons.forEach((radioButton) => {
         }
     })
 })
+
+//display articles for each page
+function displayArticlesList(articles,articlesList,currentPage,rowsCount){
+    let endIndex = currentPage * rowsCount
+    let  startIndex = endIndex - rowsCount
+
+    let paginatedArticles = articles.slice(startIndex,endIndex)
+    
+    articlesList.innerHtml = ''
+
+    paginatedArticles.forEach((article)=>{
+        articlesList.insertAdjacentHTML("beforeend",
+            `<li class="w-full h-auto max-md:flex-col max-md:items-center max-md:gap-y-4 flex gap-x-8">
+                        <img src="../images/${article.image}.png" class="w-[290px] h-[200px] rounded-[20px] bg-cover" alt="">
+                        <div class="flex flex-col max-md:items-center justify-around max-md:space-y-6">
+                            <h3 class="text-xl tracking-tighter">${article.title}</h3>
+                            <p class="leading-7 font-normal text-[#00000060] max-md:text-justify max-md:w-[300px] lg:w-[380px] text-justify line-clamp-2">${article.description}</p>
+                            <div class="flex items-center divide-x divide-[#00000010]">
+                                <div class="flex items-center px-4 gap-x-4 w-[200px]"><img src="../images/${article.profilePic}.png" class="w-[40px] h-[40px] rounded-full" alt=""> <p class="font-bold tracking-tight whitespace-nowrap">${article.authorName}</p></div>
+                                <p class="text-sm tracking-tight text-[#00000060] px-4 w-[150px]">${article.date}</p>
+                            </div>
+                        </div>
+                    </li>`
+        )
+    })
+}
+
+
+//defaul pagination setting
+window.addEventListener("DOMContentLoaded",()=>{
+    displayArticlesList(articles,articlesList,currentPage,rowsCount)
+    let paginationCount = Math.ceil(articles.length / rowsCount)
+    
+    //this pagination is static but in the future it's gonna be dynamic
+
+    // for(let i = 1;i<paginationCount;i++){
+    //     paginationList.insertAdjacentHTML('beforeend',`<button value="${i+1}" class="w-16 h-16 max-sm:w-11 max-sm:h-11 flex justify-center pageBtn items-center border-[1px] border-[#00000030] rounded-2xl cursor-pointer">${i+1}</button>`)
+    // }
+    // paginationList.removeChild(nextBtn)
+    // paginationList.appendChild(nextBtn)
+
+    if(paginationCount <= 5)
+        moreBtn.classList.add("hidden")
+})
+let flag = (articles.length / rowsCount) <= 5
+//pagination
+pagination.forEach((thisPage)=>{
+    thisPage.addEventListener("click",() => {
+        if(thisPage.value !== "1"){
+            if(flag)
+                moreBtn.classList.add("hidden")
+            previousBtn.classList.remove("hidden")
+        }else{
+            if(!previousBtn.classList.contains("hidden"))
+                previousBtn.classList.add("hidden")
+            if(currentPage.value === thisPage.value)
+                moreBtn.classList.remove("hidden")
+        }
+        thisPage.classList.add("bg-black","text-white")
+        currentPage = thisPage.value
+        articlesList.innerHTML = ''
+        if(thisPage.value !== currentBtn.value)
+            currentBtn.classList.remove("bg-black","text-white")
+        currentBtn = thisPage
+        displayArticlesList(articles,articlesList,currentPage,rowsCount)
+    })
+})
+PaginationHandler()
+// next button functionallity
+// nextBtn.addEventListener("click",() => {
+//     const nextPage = +currentPage + 1
+//     const nextPageBtn = currentBtn + 1
+//     pagination.find(btn => btn.value-1 === thisPage)
+// })
